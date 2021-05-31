1dd99a23e57655e4f9c82f805bfe5f75d9189cfb
niekcandaele <niek@candaele.dev>
2021-05-31 12:57:49 +0200

    fix: redirect handler to use correct protocol

--- binary-improvements/MapRendering/Web/Handlers/SimpleRedirectHandler.cs
+++ binary-improvements/MapRendering/Web/Handlers/SimpleRedirectHandler.cs
@@ -14,7 +14,12 @@ namespace AllocsFixes.NetConnections.Servers.Web.Handlers
         public override void HandleRequest(WebSocketSharp.Net.HttpListenerRequest _req, WebSocketSharp.Net.HttpListenerResponse _resp, WebConnection _user,
             int _permissionLevel)
         {
-            _resp.Redirect(target);
+            // There is a _resp.Redirect() method
+            // But for some reason, it redirects to a file:// url
+            // Which doesn't work (clients dont have these files locally)
+            // So we do a redirect by manually setting header and status code
+            _resp.StatusCode = (int)HttpStatusCode.Redirect;
+            _resp.SetHeader("Location", target);
         }
     }
 }
