2c49173ad9bf279e6887280ceed53089d4578eb5
niekcandaele <niek@candaele.dev>
2021-05-31 14:13:27 +0200

    feat: send JSON data + fix issue when (re)connecting

--- binary-improvements/MapRendering/Web/WSServices/LogService.cs
+++ binary-improvements/MapRendering/Web/WSServices/LogService.cs
@@ -1,17 +1,13 @@
 using WebSocketSharp;
 using WebSocketSharp.Server;
 using UnityEngine;
+using AllocsFixes.JSON;
 
 
 namespace AllocsFixes.NetConnections.Servers.Web.WSServices
 {
     public class LogService : WebSocketBehavior
     {
-
-        public LogService()
-        {
-            Logger.Main.LogCallbacks += LogCallback;
-        }
         protected override void OnMessage(MessageEventArgs e)
         {
             Send("PONG");
@@ -19,12 +15,25 @@ namespace AllocsFixes.NetConnections.Servers.Web.WSServices
 
         protected override void OnOpen()
         {
+            Logger.Main.LogCallbacks += LogCallback;
             Send("Connected");
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            Logger.Main.LogCallbacks -= LogCallback;
         }
 
         private void LogCallback(string _msg, string _trace, LogType _type)
         {
-            Send(_msg);
+            if (this.ConnectionState == WebSocketState.Open)
+            {
+                JSONObject obj = new JSONObject();
+                obj.Add("msg", new JSONString(_msg));
+                obj.Add("type", new JSONString(_type.ToString()));
+                obj.Add("trace", new JSONString(_trace));
+                Send(obj.ToString());
+            }
         }
 
     }
