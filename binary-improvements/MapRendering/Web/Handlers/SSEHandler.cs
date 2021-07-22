f35ea6af617234b2ae2d4c1edf99f8293a054ad3
niekcandaele <niek@candaele.dev>
2021-07-22 21:51:59 +0200

    feat: add more data to logs

--- binary-improvements/MapRendering/Web/Handlers/SSEHandler.cs
+++ binary-improvements/MapRendering/Web/Handlers/SSEHandler.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Net;
 using System.Text;
 using UnityEngine;
 using AllocsFixes.JSON;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 // Implemented following HTML spec
 // https://html.spec.whatwg.org/multipage/server-sent-events.html
@@ -12,6 +14,9 @@ namespace AllocsFixes.NetConnections.Servers.Web.Handlers
 {
     public class SSEHandler : PathHandler
     {
+        private static readonly Regex logMessageMatcher =
+			new Regex (@"^([0-9]{4}-[0-9]{2}-[0-9]{2})T([0-9]{2}:[0-9]{2}:[0-9]{2}) ([0-9]+[,.][0-9]+) [A-Z]+ (.*)$");
+
         private List<HttpListenerResponse> openLogResps = new List<HttpListenerResponse>();
         private readonly string moduleName;
         public SSEHandler(string _moduleName = null) : base(_moduleName)
@@ -44,13 +49,31 @@ namespace AllocsFixes.NetConnections.Servers.Web.Handlers
 
         private void LogCallback(string _msg, string _trace, LogType _type)
         {
-            StringBuilder sb = new StringBuilder();
+            LogEntry le = new LogEntry ();
+            Match match = logMessageMatcher.Match (_msg);
 
+    		if (match.Success) {
+				le.date = match.Groups [1].Value;
+				le.time = match.Groups [2].Value;
+				le.uptime = match.Groups [3].Value;
+				le.message = match.Groups [4].Value;
+			} else {
+				DateTime dt = DateTime.Now;
+				le.date = string.Format ("{0:0000}-{1:00}-{2:00}", dt.Year, dt.Month, dt.Day);
+				le.time = string.Format ("{0:00}:{1:00}:{2:00}", dt.Hour, dt.Minute, dt.Second);
+				le.uptime = "";
+				le.message = _msg;
+			}
+
+            StringBuilder sb = new StringBuilder();
             JSONObject data = new JSONObject();
-            data.Add("msg", new JSONString(_msg));
+            data.Add("msg", new JSONString(le.message));
             data.Add("type", new JSONString(_type.ToString()));
             data.Add("trace", new JSONString(_trace));
-
+            data.Add("date", new JSONString(le.date));
+            data.Add("time", new JSONString(le.time));
+            data.Add("uptime", new JSONString(le.uptime));
+            
             sb.AppendLine("event: logLine");
             sb.AppendLine($"data: {data.ToString()}");
             sb.AppendLine("");
@@ -83,5 +106,14 @@ namespace AllocsFixes.NetConnections.Servers.Web.Handlers
             }
 
         }
+
+        public class LogEntry {
+			public string date;
+			public string message;
+			public string time;
+			public string trace;
+			public LogType type;
+			public string uptime;
+		}
     }
 }
